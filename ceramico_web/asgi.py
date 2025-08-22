eedec3823ddf0a892620a0c2d93f304f09f8d96b
EVELYNADMIN <ceramicos38@gmail.com>
2025-08-22 12:30:16 -0500

    Corregir referencias de ferreteria a ceramico_web

--- ceramico_web/asgi.py
+++ ceramico_web/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ferreteria project.
+ASGI config for ceramico_web project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ferreteria.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ceramico_web.settings')
 
 application = get_asgi_application()
--- ceramico_web/wsgi.py
+++ ceramico_web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ferreteria project.
+WSGI config for ceramico_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ferreteria.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ceramico_web.settings')
 
 application = get_wsgi_application()
